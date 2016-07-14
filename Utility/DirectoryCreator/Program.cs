4ca80ba9537e235910d24866ed2580d2207413bd
prana <prana@DESKTOP-8O2H0H1>
2016-07-14 20:43:23 +1000

    test

--- Utility/DirectoryCreator/Program.cs
+++ Utility/DirectoryCreator/Program.cs
@@ -17,6 +17,7 @@ namespace DirectoryCreator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+
         }
     }
 }
